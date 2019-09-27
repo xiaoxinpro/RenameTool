36768a663fe7ff70a7ac7de69b8553c0f85cb952
Chishin <pro@xxgzs.org>
2019-09-27 16:18:53 +0800

    新增文件列表筛选下拉框初始化 版本号：0.0.0.5

--- RenameTool/FormMain.cs
+++ RenameTool/FormMain.cs
@@ -31,11 +31,16 @@ namespace RenameTool
             this.Text += @" V" + Application.ProductVersion.ToString();
 
             InitListViewFile(listViewFile);
+            InitComboFileFilter(comboFileFilter);
         }
 
         #endregion
 
         #region 文件列表相关
+        /// <summary>
+        /// 初始化文件列表
+        /// </summary>
+        /// <param name="listView">文件列表控件</param>
         private void InitListViewFile(ListView listView)
         {
             //基本属性设置
@@ -56,6 +61,30 @@ namespace RenameTool
 
             //自动列宽
             listView.Columns[5].Width = -2;//根据标题设置宽度
+        }
+
+        /// <summary>
+        /// 初始化文件列表筛选下拉框
+        /// </summary>
+        /// <param name="comboBox">下拉框控件</param>
+        /// <param name="listView">文件列表控件</param>
+        private void InitComboFileFilter(ComboBox comboBox, ListView listView = null)
+        {
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox.Items.Clear();
+            comboBox.Items.Add("全部");
+            if (listView != null)
+            {
+                foreach (ListViewItem item in listView.Items)
+                {
+                    string strExt = item.SubItems[3].Text.Trim().ToLower();
+                    if (comboBox.Items.IndexOf(strExt) < 0)
+                    {
+                        comboBox.Items.Add(strExt);
+                    }
+                }
+            }
+            comboBox.SelectedIndex = 0;
         }
         #endregion
 
