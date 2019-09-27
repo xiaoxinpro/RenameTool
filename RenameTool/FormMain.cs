6f799e968c788b1855e9c3a7df2615e0b17a7066
Chishin <pro@xxgzs.org>
2019-09-27 14:26:09 +0800

    Init Project

---
+++ RenameTool/FormMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameTool
+{
+    public partial class FormMain : Form
+    {
+        #region 初始化相关
+        /// <summary>
+        /// 窗体构造函数
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 加载窗体函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.Text += @" V" + Application.ProductVersion.ToString();
+        }
+
+        #endregion
+
+    }
+}
