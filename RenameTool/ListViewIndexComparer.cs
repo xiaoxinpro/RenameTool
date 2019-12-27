f73b73c1951f1f8060bee3f5e2ed54f51a895d44
chishin <pro@xxgzs.org>
2019-12-27 22:05:25 +0800

    增加ListView扩展类（拖拽显示相关）

---
+++ RenameTool/ListViewIndexComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameTool
+{
+    public class ListViewIndexComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((ListViewItem)x).Index - ((ListViewItem)y).Index;
+        }
+    }
+}
